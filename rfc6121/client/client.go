642b88b9fac57303febf31e7648c2f3fb70d7c45
Dominik Honnef <dominikh@fork-bomb.org>
2013-07-11 14:21:18 +0200

    basic message sending support

--- rfc6121/client/client.go
+++ rfc6121/client/client.go
@@ -26,7 +26,6 @@ func Wrap(c *client.Connection) *Connection {
 
 func (c *Connection) read() {
 	for stanza := range c.Connection.Stream {
-		spew.Dump(stanza)
 		// TODO maybe have a channel for Roster events (roster push)
 		// on which to send changes?
 
@@ -152,3 +151,63 @@ func (c *Connection) BecomeUnavailable(p *client.Presence) {
 	// TODO can't be have one global xml encoder?
 	xml.NewEncoder(c).Encode(pp)
 }
+
+func (c *Connection) SendMessage(typ, to, message string) {
+	// TODO support extended items in the mssage
+	// TODO if `to` is a bare JID, see if we know about a full JID to
+	// use instead
+	// TODO actually keep track of JIDs
+	// TODO support <thread>
+	// TODO support subject
+
+	m := client.Message{
+		Header: client.Header{
+			From: c.Connection.JID,
+			To:   to,
+			Type: typ,
+		},
+		Body: message,
+	}
+
+	xml.NewEncoder(c).Encode(m)
+}
+
+func (c *Connection) Reply(orig *client.Message, reply string) {
+	// TODO threading
+	// TODO use bare JID if full JID isn't up to date anymore
+	// TODO support subject
+	// TODO support extended items
+	c.SendMessage(orig.Type, orig.From, reply)
+}
+
+// The user's client SHOULD address the initial message in a chat
+// session to the bare JID <contact@domainpart> of the contact (rather
+// than attempting to guess an appropriate full JID
+// <contact@domainpart/resourcepart> based on the <show/>, <status/>,
+// or <priority/> value of any presence notifications it might have
+// received from the contact). Until and unless the user's client
+// receives a reply from the contact, it SHOULD send any further
+// messages to the contact's bare JID. The contact's client SHOULD
+// address its replies to the user's full JID
+// <user@domainpart/resourcepart> as provided in the 'from' address of
+// the initial message. Once the user's client receives a reply from
+// the contact's full JID, it SHOULD address its subsequent messages
+// to the contact's full JID as provided in the 'from' address of the
+// contact's replies, thus "locking in" on that full JID. A client
+// SHOULD "unlock" after having received a <message/> or <presence/>
+// stanza from any other resource controlled by the peer (or a
+// presence stanza from the locked resource); as a result, it SHOULD
+// address its next message(s) in the chat session to the bare JID of
+// the peer (thus "unlocking" the previous "lock") until it receives a
+// message from one of the peer's full JIDs.
+
+// When two parties engage in a chat session but do not share presence
+// with each other based on a presence subscription, they SHOULD send
+// directed presence to each other so that either party can easily
+// discover if the peer goes offline during the course of the chat
+// session. However, a client MUST provide a way for a user to disable
+// such presence sharing globally or to enable it only with particular
+// entities. Furthermore, a party SHOULD send directed unavailable
+// presence to the peer when it has reason to believe that the chat
+// session is over (e.g., if, after some reasonable amount of time, no
+// subsequent messages have been exchanged between the parties).
