caf97b3a200373aaad3f49e81c20fe0ae0eac5d7
Dominik Honnef <dominikh@fork-bomb.org>
2013-08-07 08:00:16 +0200

    gofmt

--- shared/rfc6120/resolve.go
+++ shared/rfc6120/resolve.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-        DefaultClientPort = 5222 // Default port for client-to-server connections
-        DefaultServerPort = 5269 // Default port for server-to-server connections
+	DefaultClientPort = 5222 // Default port for client-to-server connections
+	DefaultServerPort = 5269 // Default port for server-to-server connections
 )
 
 // consider renaming this type
